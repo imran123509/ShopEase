e17d07bffe79f7189e476d8f7185907725e89419
WOW ROOMS <wowrooms@WOWs-MacBook-Pro.local>
2024-02-16 18:24:29 +0530

    added all frontend

--- src/pages/AdminHome.js
+++ src/pages/AdminHome.js
@@ -1,11 +1,11 @@
-//import AdminProductList from "../features/admin/components/AdminProductList";
+import AdminProductList from "../features/admin/components/AdminProductList";
 import NavBar from "../features/navbar/Navbar";
 
 function AdminHome() {
     return ( 
         <div>
             <NavBar>
-                {/* <AdminProductList></AdminProductList> */}
+                <AdminProductList></AdminProductList>
             </NavBar>
         </div>
      );
