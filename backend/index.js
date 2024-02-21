92bb46c7ef0dd4bf640bfc54d5cf9f7d15fa2142
WOW ROOMS <wowrooms@WOWs-MacBook-Pro.local>
2024-02-21 12:54:53 +0530

    added some backend

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
 const express=require('express');
 const mongoose=require('mongoose');
+const cors=require('cors')
 //const createProduct=require('./controller/Product')
 //const { createProduct } = require('./controller/Product');
+const brandsRouter=require('./routes/Brand')
 
 const productRouters=require('./routes/product')
 
@@ -14,8 +16,10 @@ async function main(){
 const app=express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/', productRouters.router);
+app.use('/brands', brandsRouter.router);
 
 app.listen(8080, ()=>{
       console.log(`server is connect ${8080}`)
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,8 +1,20 @@
 const express=require('express');
-const { createProduct } = require('../controller/Product');
+const { createProduct, fetchAllProducts, fetchProductById, updateProduct } = require('../controller/Product');
 
 
 const router=express.Router();
 
-router.post('/products', createProduct)
+router.post('/products', createProduct).get('/', fetchAllProducts).get('/', fetchProductById)
+.patch('/', updateProduct)
+
+// .get('/update/test',async(req,res)=>{
+      //       // For adding discountPrice to existing data : delete this code after use
+      //      const products = await Product.find({});
+      //      for(let product of products){
+      //       product.discountPrice = Math.round(product.price*(1-product.discountPercentage/100))
+      //       await product.save()
+      //       console.log(product.title+ ' updated')
+      //      }
+      //      res.send('ok')
+      // })
 exports.router=router;
